5cc00e51193ae1346e766d7df64093b8131914f2
Mauricio Oliveira <mau_oliver@hotmail.com>
2023-12-04 10:26:31 -0300

    add app to test router dom

---
+++ src/index.jsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from './routes/root'
+import ErrorPage from "./error-page";
+import Product from './product';
+import Contact from "./routes/contact";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/product",
+    element:<Product />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
+  },
+  
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/product.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div>Adicionar Produto</div>
+  )
+}
+
+export default Product;
